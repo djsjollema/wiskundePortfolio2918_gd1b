6dfe3352ecd40d441ab384938ecd8c22f0ae114c
Jelle Sjollema <j.sjollema@ma-web.nl>
2018-05-25 13:55:46 +0200

    balls

--- html/ping/script.js
+++ html/ping/script.js
@@ -4,33 +4,41 @@ const context = canvas.getContext('2d');
 canvas.width = window.innerWidth;
 canvas.height = window.innerHeight;
 
-let ball = new Point(200,200,50,"red");
-let speedX = 1;
-let speedY = 1;
-ball.draw(context)
+let numberOfBalls = 10;
+let balls = [];
 
-function animate(){
+for (let i = 0; i < numberOfBalls; i++) {
+  let object = {};
+  object.ball = new Point(200, 200, 50, "red");
+  object.speedX = Math.random() * 2;
+  object.speedY = Math.random() * 2;
+  balls.push(object);
+}
+
+function animate() {
   requestAnimationFrame(animate);
-  context.clearRect(0,0,canvas.width,canvas.height);
-  ball.draw(context);
-  ball.x += speedX;
-  ball.y += speedY;
+  context.clearRect(0, 0, canvas.width, canvas.height);
+  for (let i = 0; i < balls.length; i++) {
+    balls[i].ball.draw(context);
+    balls[i].ball.x += balls[i].speedX;
+    balls[i].ball.y += balls[i].speedY;
 
-  if(ball.y > canvas.height-ball.r){
-    speedY = -speedY;
+    if (balls[i].ball.y > canvas.height - balls[i].ball.r) {
+      balls[i].speedY = -balls[i].speedY;
+    }
+
+    if (balls[i].ball.x > canvas.width - balls[i].ball.r) {
+      balls[i].speedX = -balls[i].speedX;
+    }
+
+    if (balls[i].ball.y < balls[i].ball.r) {
+      balls[i].speedY = -balls[i].speedY;
+    }
+    if (balls[i].ball.x < balls[i].ball.r) {
+      balls[i].speedX = -balls[i].speedX;
+    }
+
   }
-
-  if(ball.x > canvas.width-ball.r){
-    speedX = -speedX;
-  }
-
-  if(ball.y <ball.r){
-    speedY = - speedY;
-  }
-
-  if(ball.x < ball.r){
-    speedX = -speedX;
-  };
 }
 
 animate();
