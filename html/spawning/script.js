15eb97c56da125e52c89ef6b5e4ec2a5c891ebd5
Jelle Sjollema <j.sjollema@ma-web.nl>
2018-06-22 12:10:29 +0200

    spawning starting1

--- html/spawning/script.js
+++ html/spawning/script.js
@@ -1,1 +1,7 @@
-console.log('hallo wereld');
+const canvas = document.getElementById('canvas');
+const context = canvas.getContext('2d');
+
+canvas.width = window.innerWidth;
+canvas.height = window.innerHeight;
+
+context.fillRect(0,0,1500,1500);
