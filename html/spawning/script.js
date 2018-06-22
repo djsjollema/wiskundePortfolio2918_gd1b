d85ddbf97261ed71d77f61acf7f129cfe2e81f6c
Jelle Sjollema <j.sjollema@ma-web.nl>
2018-06-22 13:29:32 +0200

    spawning balls added

--- html/spawning/script.js
+++ html/spawning/script.js
@@ -4,23 +4,36 @@ const context = canvas.getContext('2d');
 canvas.width = window.innerWidth;
 canvas.height = window.innerHeight;
 
-let ball = {};
-ball.point = new Point(500,400,100,"DarkBlue");
-ball.pos = new Vector2d(800,500);
-ball.vel = new Vector2d(8,7);
+let numBalls = 10;
+let balls = [];
+
+for(let i = 0; i< numBalls; i++){
+  let ball = {};
+  ball.point = new Point(0,0,5,"DarkBlue");
+  ball.pos = new Vector2d(randomNumber(0,canvas.width),randomNumber(0,canvas.height));
+  ball.vel = new Vector2d(8,7);
+  balls.push(ball);
+}
+
+function randomNumber(min,max){
+  return Math.random()*(max - min) + min;
+}
 
 function animate(){
   requestAnimationFrame(animate);
   context.clearRect(0,0,canvas.width,canvas.height);
-  ball.pos.add(ball.vel);
-  ball.point.pos(ball.pos);
-  ball.point.draw(context);
-  if(ball.pos.dx < ball.point.r || ball.pos.dx > canvas.width - ball.point.r){
-    ball.vel.dx = -ball.vel.dx;
+  for (var i = 0; i < balls.length; i++) {
+    balls[i].pos.add(balls[i].vel);
+    balls[i].point.pos(balls[i].pos);
+    balls[i].point.draw(context);
+    if(balls[i].pos.dx < balls[i].point.r || balls[i].pos.dx > canvas.width - balls[i].point.r){
+      balls[i].vel.dx = -balls[i].vel.dx;
+    }
+    if(balls[i].pos.dy < balls[i].point.r || balls[i].pos.dy > canvas.height-balls[i].point.r){
+      balls[i].vel.dy = -balls[i].vel.dy;
+    }
   }
-  if(ball.pos.dy < ball.point.r || ball.pos.dy > canvas.height-ball.point.r){
-    ball.vel.dy = -ball.vel.dy;
-  }
+
 }
 
 animate();
