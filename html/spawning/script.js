a8b9bd4d8900b9df1d213483ea8f46f6b3e30f69
Jelle Sjollema <j.sjollema@ma-web.nl>
2018-06-22 13:47:15 +0200

    spawning balls final

--- html/spawning/script.js
+++ html/spawning/script.js
@@ -4,16 +4,16 @@ const context = canvas.getContext('2d');
 canvas.width = window.innerWidth;
 canvas.height = window.innerHeight;
 
-let numBalls = 10;
+let numBalls = 1000;
 let balls = [];
 
-for(let i = 0; i< numBalls; i++){
-  let ball = {};
-  ball.point = new Point(0,0,5,"DarkBlue");
-  ball.pos = new Vector2d(randomNumber(0,canvas.width),randomNumber(0,canvas.height));
-  ball.vel = new Vector2d(8,7);
-  balls.push(ball);
-}
+// for(let i = 0; i< numBalls; i++){
+//   let ball = {};
+//   ball.point = new Point(0,0,5,"DarkBlue");
+//   ball.pos = new Vector2d(randomNumber(0,canvas.width),randomNumber(0,canvas.height));
+//   ball.vel = new Vector2d(randomNumber(-10,10),randomNumber(-10,10));
+//   balls.push(ball);
+// }
 
 function randomNumber(min,max){
   return Math.random()*(max - min) + min;
@@ -21,16 +21,29 @@ function randomNumber(min,max){
 
 function animate(){
   requestAnimationFrame(animate);
-  context.clearRect(0,0,canvas.width,canvas.height);
+  if(Math.random()<0.1){
+      let ball = {};
+      ball.point = new Point(0,0,5,"DarkBlue");
+      ball.pos = new Vector2d(randomNumber(0,canvas.width),-10);
+      ball.vel = new Vector2d(0,randomNumber(0,15));
+      balls.push(ball);
+  }
+  //context.clearRect(0,0,canvas.width,canvas.height);
+  context.fillStyle= "rgba(120,120,255,0.05)"
+  context.fillRect(0,0,canvas.width,canvas.height)
   for (var i = 0; i < balls.length; i++) {
     balls[i].pos.add(balls[i].vel);
     balls[i].point.pos(balls[i].pos);
     balls[i].point.draw(context);
-    if(balls[i].pos.dx < balls[i].point.r || balls[i].pos.dx > canvas.width - balls[i].point.r){
-      balls[i].vel.dx = -balls[i].vel.dx;
-    }
-    if(balls[i].pos.dy < balls[i].point.r || balls[i].pos.dy > canvas.height-balls[i].point.r){
-      balls[i].vel.dy = -balls[i].vel.dy;
+    // if(balls[i].pos.dx < balls[i].point.r || balls[i].pos.dx > canvas.width - balls[i].point.r){
+    //   balls[i].vel.dx = -balls[i].vel.dx;
+    // }
+    // if(balls[i].pos.dy < balls[i].point.r ){
+    //   balls[i].vel.dy = -balls[i].vel.dy;
+    // }
+
+    if(balls[i].pos.dy > canvas.height-balls[i].point.r){
+      balls.splice(i,1);
     }
   }
 
